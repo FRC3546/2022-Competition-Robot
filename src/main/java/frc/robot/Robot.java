09a475f6627d4b72b80c2f1d5406fb0294e81bdc
Connor Fleser <cfleser11@gmail.com>
2022-02-07 18:10:58 -0500

    nothing

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -18,14 +18,8 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.cameraserver.CameraServer;
-import com.kauailabs.navx.frc.AHRS;
-import edu.wpi.first.wpilibj.SerialPort;
-
-import edu.wpi.first.wpilibj.shuffleboard.*;
-
 
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -34,129 +28,79 @@ import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
  * project.
  */
 
+
+
 public class Robot extends TimedRobot {
-  private DifferentialDrive drive_train;
+  private DifferentialDrive robot_motors;
   
-  // These are the definitions of the actual joysticks
-  private Joystick left_driver_controller= new Joystick(0);
-  private Joystick right_driver_controller = new Joystick(1);
-  private Joystick codriver_controller = new Joystick(2);
-  
-  // Driver left controller buttons
-  private JoystickButton IntakeButton = new JoystickButton(right_driver_controller, 1);
-  private JoystickButton DriveTrainReturnButton = new JoystickButton(right_driver_controller, 8);
-  private JoystickButton DriveTrainInvertButton = new JoystickButton(right_driver_controller, 7);
+  private Joystick left_driver_controller;
+  private Joystick right_driver_controller;
+  private Joystick codriver_controller;
 
-  
-  // Driver right controller buttons
-  private JoystickButton GyroResetButton = new JoystickButton(left_driver_controller, 10);
 
-  // Co-driver buttons
-  private JoystickButton ShooterOnButton = new JoystickButton(codriver_controller, 6);
-  private JoystickButton ShooterOffButton = new JoystickButton(codriver_controller, 4);
-  private JoystickButton ConveyorForwardButton = new JoystickButton(codriver_controller, 8);
-  private JoystickButton ConveyorReverseButton = new JoystickButton(codriver_controller, 12);
-  private JoystickButton ConveyorStopButton = new JoystickButton(codriver_controller, 10);
-  private JoystickButton ClimberTiltButton = new JoystickButton(codriver_controller, 11);
-  private JoystickButton ClimberReturnButton = new JoystickButton(codriver_controller, 9);
+  //Left Driver Buttons
+  // private JoystickButton Intake;
+  // private JoystickButton Lbutton2;
+  // private JoystickButton Lbutton3;
+  // private JoystickButton Lbutton4;
+  // private JoystickButton Lbutton5;
+  // private JoystickButton Lbutton6;
+  // private JoystickButton Lbutton7;
+  // private JoystickButton Lbutton8;
 
-  private DoubleSolenoid Intake_Solenoid = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 0, 1);
-  private DoubleSolenoid CargoRelease_Solenoid = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 2, 3);
-  private DoubleSolenoid Climber_Solenoid = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 4, 5);
+  //Right Driver Buttons
+  // private JoystickButton Rbutton1;
+  // private JoystickButton Rbutton2;
+  // private JoystickButton Rbutton3;
+  // private JoystickButton Rbutton4;
+  // private JoystickButton Rbutton5;
+  // private JoystickButton Rbutton6;
+  // private JoystickButton Rbutton7;
+  // private JoystickButton Rbutton8;
+
+  //Codriver Buttons
+  private JoystickButton climbExtend;
+  private JoystickButton climbRetract;
+  private JoystickButton shooter;
+  private JoystickButton conveyor;
+  private JoystickButton escapmentRetract;
+  private JoystickButton escapmentExtend;
+  private JoystickButton raiseIntake;
+  private JoystickButton lowerIntake;
+  // private JoystickButton Cobutton9;
+  // private JoystickButton Cobutton10;
+  // private JoystickButton Cobutton11;
+  // private JoystickButton Cobutton12;
+
+  private DoubleSolenoid IntakeSolenoid;
+  private DoubleSolenoid EscapementSolenoid;
+  private DoubleSolenoid Climber1Solenoid;
+  private DoubleSolenoid Climber2Solenoid;
+
 
   private final Timer timer = new Timer();
 
+  private static final String kDefaultAuto = "Default";
+  private static final String kRightSide = "Right Side";
+  private static final String kLeftSide = "Left Side";
+  
   private String m_autoSelected;
   private String m_autoCargo;
-
-  private static final String GetCargo = "Get the Cargo";
-  private static final String LeaveTarmac = "Leave Tarmac";
-  private static final String Nothing = "Do Nothing";
-  private final SendableChooser<String> m_routines = new SendableChooser<>();
+  private final SendableChooser<String> m_chooser = new SendableChooser<>();
   
-  private static final String WallCargo = "Wall Cargo";
-  private static final String TerminalCargo = "Terminal Cargo";
-  private static final String HangarCargo = "Hanger Cargo";
+  private static final String kRightCargo = "Right Cargo";
+  private static final String kLeftCargo = "Left Cargo";
+
   private final SendableChooser<String> m_cargochooser = new SendableChooser<>();
 
-  private VictorSP left_motor = new VictorSP(0);
-  private VictorSP right_motor = new VictorSP(2);
+  private VictorSP left_motor_control = new VictorSP(0);
+  private VictorSP right_motor_control = new VictorSP(2);
 
-  private VictorSP intake_motor = new VictorSP(4);
-  private VictorSP conveyor_motor = new VictorSP(6);
   private Spark climber_extension = new Spark(7);
-  private CANSparkMax shooter_motor = new CANSparkMax(28, MotorType.kBrushless);
-
-  AHRS gyro = new AHRS(SerialPort.Port.kUSB);
-
-  final double kP = 1;
-
-
-     // Methods for the Conveyor
-     public void ActivateConveyor()
-     {
-      conveyor_motor.set(1);
-    }
-    
-    public void DeactivateConveyor()
-    {
-      conveyor_motor.set(0);
-    }
-    
-    public void ReverseConveyor()
-    {
-      conveyor_motor.set(-0.5);
-    } 
-  
-
-    // Methods for the Shooter
-    public void ActivateShooterMotor()
-    {
-     shooter_motor.set(1);
-    }
-  
-    public void DeactivateShooterMotor()
-    {
-      shooter_motor.set(0);
-    }
-  
-
-    // Methods for the Climber
-    public void TiltClimber()
-    {
-      Climber_Solenoid.set(Value.kForward);
-    }
-  
-    public void ReturnClimber()
-    {
-      Climber_Solenoid.set(Value.kReverse);
-    }
-
-  
-    // Methods for the Cargo
-    public void ReleaseCargo()
-    {
-      CargoRelease_Solenoid.set(Value.kReverse);
-    }
-  
-    public void StopCargo()
-    {
-      CargoRelease_Solenoid.set(Value.kForward);
-    }
-
-
-    // Methods for Intake
-    public void ActivateIntake()
-    {
-      intake_motor.set(1);
-      Intake_Solenoid.set(Value.kForward);
-    }
-
-    public void DeactivateIntake()
-    {
-      Intake_Solenoid.set(Value.kReverse);
-      intake_motor.set(0);
-    }
+  private VictorSP outer_intake_motor = new VictorSP(4);
+  private VictorSP inner_intake_motor = new VictorSP(5);
+  private VictorSP conveyor_motor = new VictorSP(6);
+  // private CANSparkMax shooter_motor = new CANSparkMax(28, MotorType.kBrushless);
 
   /**
    * This function is run when the robot is first started up and should be used for any
@@ -164,31 +108,60 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
+    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
+    m_chooser.addOption("Right Side", kRightSide);
+    m_chooser.addOption("Left Side", kLeftSide);
+    SmartDashboard.putData("Robot Tarmac Options", m_chooser);
 
-    left_motor.setInverted(true);
-    right_motor.setInverted(true);
-
-    m_routines.setDefaultOption("Get the Cargo", GetCargo);
-    m_routines.addOption("Leave Tarmac", LeaveTarmac);
-    m_routines.addOption("Do nothing", Nothing);
-    SmartDashboard.putData("Robot Routines", m_routines);
-
-    m_cargochooser.addOption("Wall Cargo", WallCargo);
-    m_cargochooser.addOption("Terminal Cargo", TerminalCargo);
-    m_cargochooser.addOption("Hangar Cargo", HangarCargo);
+    m_cargochooser.addOption("Right Cargo", kRightCargo);
+    m_cargochooser.addOption("Left Cargo", kLeftCargo);
     SmartDashboard.putData("Cargo Options", m_cargochooser);
 
-    SmartDashboard.putData("Gyro", gyro);
-    Shuffleboard.getTab("Example tab").add(gyro);
+
+    robot_motors = new DifferentialDrive(left_motor_control, right_motor_control);
+
+    left_driver_controller= new Joystick(0);
+    right_driver_controller = new Joystick(1);
+
+    codriver_controller = new Joystick(2);
+
+    // Lbutton1 = new JoystickButton(left_driver_controller, 1);
+    // Lbutton2 = new JoystickButton(left_driver_controller, 2);
+    // Lbutton3 = new JoystickButton(left_driver_controller, 3);
+    // Lbutton4 = new JoystickButton(left_driver_controller, 4);
+    // Lbutton5 = new JoystickButton(left_driver_controller, 5);
+    // Lbutton6 = new JoystickButton(left_driver_controller, 6);
+    // Lbutton7 = new JoystickButton(left_driver_controller, 7);
+    // Lbutton8 = new JoystickButton(left_driver_controller, 8);
+
+    JoystickButton Intake = new JoystickButton(right_driver_controller, 1);
+    // Rbutton2 = new JoystickButton(right_driver_controller, 2);
+    // Rbutton3 = new JoystickButton(right_driver_controller, 3);
+    // Rbutton4 = new JoystickButton(right_driver_controller, 4);
+    // Rbutton5 = new JoystickButton(right_driver_controller, 5);
+    // Rbutton6 = new JoystickButton(right_driver_controller, 6);
+    // Rbutton7 = new JoystickButton(right_driver_controller, 7);
+    // Rbutton8 = new JoystickButton(right_driver_controller, 8);
 
 
-    drive_train = new DifferentialDrive(left_motor, right_motor);
+    shooter = new JoystickButton(codriver_controller, 1);
+    conveyor = new JoystickButton(codriver_controller, 2);
+    // Lbutton5 = new JoystickButton(codriver_controller, 3);
+    // Lbutton6 = new JoystickButton(codriver_controller, 4);
+    // Lbutton7 = new JoystickButton(codriver_controller, 5);
+    // Lbutton8 = new JoystickButton(codriver_controller, 6);
+    raiseIntake = new JoystickButton(codriver_controller, 7);
+    lowerIntake = new JoystickButton(codriver_controller, 8);
+    escapmentRetract = new JoystickButton(codriver_controller, 9);
+    escapmentExtend = new JoystickButton(codriver_controller, 10);
+    climbExtend = new JoystickButton(codriver_controller, 11);
+    climbRetract = new JoystickButton(codriver_controller, 12);
 
-
-
-    ReturnClimber();
-    StopCargo();
-
+    IntakeSolenoid = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 0, 1);
+    EscapementSolenoid = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 2, 3);
+    Climber1Solenoid = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 4, 5);
+    Climber2Solenoid = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 6, 7);
+        
     CameraServer.startAutomaticCapture();
 
   }
@@ -215,7 +188,7 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void autonomousInit() {
-    m_autoSelected = m_routines.getSelected();
+    m_autoSelected = m_chooser.getSelected();
     m_autoCargo = m_cargochooser.getSelected();
     // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
     System.out.println("Auto selected: " + m_autoSelected);
@@ -229,29 +202,31 @@ public class Robot extends TimedRobot {
   @Override
   public void autonomousPeriodic() {
     switch (m_autoSelected) {
-      case GetCargo:
+      case kDefaultAuto:
       default:
-        if (m_autoCargo == TerminalCargo){
-
+        // Put default auto code here
+        break;
+      case kRightSide:
+        if (m_autoCargo == kRightCargo){
+          robot_motors.tankDrive(0.5, 0.5);
         }
         
-        if (m_autoCargo == HangarCargo){
-
-        }
-
-        if (m_autoCargo == WallCargo){
-
-        }
-        break;
-
-      case LeaveTarmac:{
-        drive_train.tankDrive(0.5, 0.5);
+        if (m_autoCargo == kLeftCargo){
+          robot_motors.tankDrive(-0.5, -0.5);
         }
         break;
       
-        case Nothing:{      
-            // Does nothing LOL
-        }
+        case kLeftSide:
+          if (m_autoCargo == kRightCargo){
+            robot_motors.tankDrive(0.5, -0.5);
+          }
+          
+          if (m_autoCargo == kRightCargo){
+            robot_motors.tankDrive(-0.5, 0.5);
+            if(timer.get() == 5){
+              robot_motors.stopMotor();
+            }
+          }
         
         break;
     }
@@ -265,87 +240,9 @@ public class Robot extends TimedRobot {
   @Override
   public void teleopPeriodic() {
 
-    drive_train.tankDrive(left_driver_controller.getY(), right_driver_controller.getY());
+    robot_motors.tankDrive(left_driver_controller.getY(), right_driver_controller.getY());
 
-
-      if(IntakeButton.get())
-      {
-        intake_motor.set(1);
-      }
-      else{
-        intake_motor.set(0);
-      }
-
-      if(DriveTrainInvertButton.get())
-      {
-
-      }
-      
-      
-      if(DriveTrainReturnButton.get())
-      {
-        
-      }
-
-      
-      if(GyroResetButton.get())
-      {
-        gyro.zeroYaw();
-      }
-
-
-      if(ShooterOnButton.get())
-      {
-        ActivateShooterMotor();
-      }
-
-
-      if(ShooterOffButton.get())
-      {
-        DeactivateIntake();
-      }
-
-
-      if(ConveyorForwardButton.get())
-      {
-        ActivateConveyor();
-      }
-
-
-      if(ConveyorReverseButton.get())
-      {
-        ReverseConveyor();
-      }
-
-      if(ConveyorStopButton.get())
-      {
-        DeactivateConveyor();
-      }
-
-
-      if(ClimberTiltButton.get())
-      {
-        TiltClimber();
-      }
-
-
-      if(ClimberReturnButton.get())
-      {
-        ReturnClimber();
-      }
-
-
-
-
-    // if(right_driver_controller.getRawButton(12))
-    // {
-    // climber_extension.set(.9);
-    // }
-    // else
-    // {
-    // climber_extension.set(0);
-    // }
-      if(codriver_controller.getRawButton(12))
+      if(codriver_controller.getRawButton())
       {
         System.out.println("button 12 pushed");
       }
